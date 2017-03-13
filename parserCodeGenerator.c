af11a6ae08276f302e1bbfa0ec66ff6ebc0125b0
philiprodriguez <philiprodriguez@knights.ucf.edu>
2017-03-13 09:57:47 -0400

    ...

--- parserCodeGenerator.c
+++ parserCodeGenerator.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 #define MAX_INPUT_SIZE 32768
 
-//A method to halt execution after presenting the
-// error corresponding to the code [code].
+/*
+	This method halts the program execution after printing an error 
+	with code [code].
+*/
 void error(int code)
 {
 	if (code == 1)
@@ -65,24 +68,91 @@ void error(int code)
 FILE * inputFile;
 FILE * outputFile;
 
+/*
+	This method will simply open the files, and thus populate
+	the variables inputFile and outputFile with meaningful
+	addresses.
+*/
 void openFiles()
 {
-	inputFile = fopen("input.txt", "r");
+	inputFile = fopen("laOutput.txt", "r");
 	outputFile = fopen("output.txt", "w");
 }
 
 //The actual input file data handling...
 char lexemeList[MAX_INPUT_SIZE];
 
-void readInputFile()
+/*
+	Clears the lexemeList array out with all null terminators.
+*/
+void clearLexemeList()
 {
-	//TODO: find the lexeme list and read only the lexeme list data into lexemeList. This information should be exclusively on the last line of the output from lexicalAnalyzer.
+	for(int i = 0; i < MAX_INPUT_SIZE; i++)
+	{
+		lexemeList[i] = '\0';
+	}
 }
 
+/*
+	This method will read from the file pointed to by inputFile, and
+	populate the variable lexemeList with the last line from the inputFile.
+*/
+void readInputFile()
+{
+	//Clear the lexeme list because we're about to write to it
+	clearLexemeList();
+
+	//Seek to the end and get the length of the input file in characters...
+	fseek(inputFile, 0, SEEK_END);
+	int inputSize = ftell(inputFile)-1;
+	
+	//Seek back to the beginning
+	fseek(inputFile, 0, SEEK_SET);
+	
+	//Iterate character by character through the file...
+	int i, place = 0;
+	for(i = 0; i < inputSize; i++)
+	{
+		//Read a character into lexemeList at place...
+		fscanf(inputFile, "%c", &lexemeList[place]);
+		
+		//Was that character a newline? If so, clear the 
+		//lexemeList (because there must be another line below)
+		if (lexemeList[place] == '\n')
+		{
+			place = -1;
+			clearLexemeList();
+		}
+		
+		//Increment place...
+		place++;
+	}
+	
+	//Make sure our lexemeList string has a null terminator at the end!
+	lexemeList[place] = '\0';
+}
+
+
+
+// --------------------------------------------------Begin Parser/Code Generator Code--------------------------------------------------
+
+void parseLexemes()
+{
+	
+}
+
+// --------------------------------------------------End Parser/Code Generator Code--------------------------------------------------
+
+/*
+	I expect this program to take an input file of lexemes, parse it, and print out
+	a file containing whether or not parsing succeeded, and then the generated code.
+*/
 int main()
 {
 	//TODO: Later, we can make the arguments determine the input and output file paths, but for now it's hardcoded as input.txt and output.txt.
 	openFiles();
 	readInputFile();
+	parseLexemes();
+	
 	return 0;
 }
